5906d936351b4055bdac8905d51b136557ed9854
KANDY228 <71720867+KANDY228@users.noreply.github.com>
2022-08-31 14:50:22 +0300

    DataOnly
    DataOnly

---
+++ src/main/java/MAIN/DataOnly/App.java
@@ -0,0 +1,11 @@
+package MAIN.DataOnly;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    DataOnly c1;
+    c1 = new DataOnly("31.08.2022");
+    System.out.println("App: Main: c1.getDataOnly() ="+ c1.getDataOnly());
+    }
+}
---
+++ src/main/java/MAIN/DataOnly/DataOnly.java
@@ -0,0 +1,12 @@
+package MAIN.DataOnly;
+
+public class DataOnly {
+	String DataOnly = "5";
+	public DataOnly(String d_arg) {
+		DataOnly = d_arg;
+	}
+	public String getDataOnly() {
+		return DataOnly;
+	}
+
+}
